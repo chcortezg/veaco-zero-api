5bda128584548498359b47400523b81ba977fd5b
chcortezg <chcortezgomes@gmail.com>
2025-11-23 12:14:41 -0300

    Initial commit: Estrutura inicial gerada pelo Spring Initializr

---
+++ src/main/java/com/veacozero/api/VeacoZeroApiApplication.java
@@ -0,0 +1,13 @@
+package com.veacozero.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeacoZeroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeacoZeroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veacozero/api/VeacoZeroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veacozero.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeacoZeroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
